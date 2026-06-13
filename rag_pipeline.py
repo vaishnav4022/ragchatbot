7f484805019ed2dafd0718c6e12e7d6889aab09b
Vaishnav Aglawe <vaishnavaglave@vaishnavs-MacBook-Air.local>
2026-06-13 19:21:14 +0530

    two files updated

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -12,7 +12,7 @@ import os
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
